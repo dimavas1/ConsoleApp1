e179f7b027f57069d0852ba48a0592612a12a232
dimavas1 <dima.vasserfirer@gmail.com>
2021-09-12 17:06:02 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine(reverse("hellow!"));
+		}
+
+		static string reverse(string str)
+		{
+			string temp = "";
+			for (int i = 0; i < str.Length; i++)
+			{
+				temp = temp + str[str.Length - 1 - i];
+			}
+
+			return temp;
+		}
+
+	}
+}
