96da453d024a810f890ec4981041c677887f76dd
dimavas1 <dima.vasserfirer@gmail.com>
2021-09-12 19:31:33 -0500

    High/Low Number Guessing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,10 +7,25 @@ namespace ConsoleApp1
 
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string play = "y";
+			
 			Console.WriteLine(Reverse("Hello World!"));
-            double aSurface = CalculateRectangular();
+            
+			double aSurface = CalculateRectangular();
+            
 			Calculatelabor(aSurface);
+            Console.WriteLine("Guessing game");
+			
+			
+
+            while(play=="y")
+            {
+				GuessTheNumber();
+				Console.WriteLine("Do you want to play again y/n");
+				play = Console.ReadLine().ToLower();
+			}
+			
+			
 		}
 
         static string Reverse(string str)
@@ -50,6 +65,53 @@ namespace ConsoleApp1
 			Console.WriteLine("Labor Estimation 20 sqf/h for 86$ ");
 			Console.WriteLine($"Estimated labour cost is: {Math.Round((aSurface/20)*86,2)} $");
 			Console.WriteLine($"Estimated labour hours is: {Math.Round(aSurface/20,2)} h");
+		}
+
+		static void GuessTheNumber()
+        {
+			int nTrials = 10;
+			string tHight = "Your guess is too hight";
+			string hight = "Your guess is hight";
+			string tLow = "Your guess is too low";
+			string low = "Your guess is low";
+			string gEnd = "You Lose";
+			string yWin = "You Win";
+			int tNum;
+
+			Random rnd = new Random();
+
+			int rNumber = rnd.Next(1, 100);
+
+            for (int i = nTrials; i > 0; i--)
+            {
+				Console.WriteLine("Guess a number between 1 and 100");
+
+				tNum = int.Parse(Console.ReadLine());
+
+                if (tNum==rNumber)
+                {
+					Console.WriteLine(yWin);
+					break;
+				}
+                else if(tNum - rNumber > 10) 
+                {
+					Console.WriteLine(tHight);
+				}
+				else if (tNum - rNumber < 10 && tNum-rNumber > 0)
+				{
+					Console.WriteLine(hight);
+				}
+				else if (tNum - rNumber > -10 && tNum - rNumber < 0)
+				{
+					Console.WriteLine(low);
+				}
+				else
+                {
+					Console.WriteLine(tLow);
+				}
+			}
+
+
 		}
 
 	}
